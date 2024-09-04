01b0eef0e347c7cce3917ac1255b19939ea06aa7
Michael <michaelbarnes7282@gmail.com>
2024-09-04 12:41:49 -0700

    fresh home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,6 @@
-import {
-  Table,
-  TableHeader,
-  TableBody,
-  TableColumn,
-  TableRow,
-  TableCell
-} from "@nextui-org/table";
-
 
 export default function Home() {
   return (
-    <Table aria-label="Example table with dynamic content">
-      <TableHeader>
-        <TableColumn>Name</TableColumn>
-      </TableHeader>
-      <TableBody>
-        <TableRow key="1">
-          <TableCell>Gun That's Big</TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+    <h1>Hello World!</h1>
   );
 }
